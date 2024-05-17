6a78d1ef8d50dc4de253284e6425f3dc8d9d8e74
0verdoz <wmakpodo@st.ug.edu.gh>
2024-05-17 20:25:40 +0100

    Task 1 correction

--- arrayManipulation.js
+++ arrayManipulation.js
@@ -1,19 +1,12 @@
+// arraymanipulation.js
 function processArray(numArray) {
     return numArray.map (num => 
         (num % 2 === 0 ? num * num : num * 3)
     );
 }
 
-
-function formatArrayStrings(strArray , numArray) {
-    return strArray.map ((str, index) =>
-        (numArray[index] % 2 === 0 ? str.toUpperCase() : str.toLowerCase())
-    );
-
-}
+// Task 1: processArray function
 
 let selection = processArray([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
 console.log(selection);
 
-let wordSelection = formatArrayStrings(['Tuvalu' , 'Timor-Leste' , 'Bhutan', 'Belize', 'Andorra', 'Kiribati', 'Vanuatu', 'Suriname', 'Palau', 'Guyana'] , [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
-console.log(wordSelection);
