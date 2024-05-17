e79e08aa7dc60656cdd4fbbf190e4d615a760c74
0verdoz <wmakpodo@st.ug.edu.gh>
2024-05-17 20:28:50 +0100

    Tried to separate Task 1 and Task 2.

--- arrayManipulation.js
+++ arrayManipulation.js
@@ -1,12 +1,19 @@
-// arraymanipulation.js
 function processArray(numArray) {
     return numArray.map (num => 
         (num % 2 === 0 ? num * num : num * 3)
     );
 }
 
-// Task 1: processArray function
+
+function formatArrayStrings(strArray , numArray) {
+    return strArray.map ((str, index) =>
+        (numArray[index] % 2 === 0 ? str.toUpperCase() : str.toLowerCase())
+    );
+
+}
 
 let selection = processArray([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
 console.log(selection);
 
+let wordSelection = formatArrayStrings(['Tuvalu' , 'Timor-Leste' , 'Bhutan', 'Belize', 'Andorra', 'Kiribati', 'Vanuatu', 'Suriname', 'Palau', 'Guyana'] , [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+console.log(wordSelection);
