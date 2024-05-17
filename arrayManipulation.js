d0b99eeb837394c1c560e81c9fd37742f01e17c1
0verdoz <wmakpodo@st.ug.edu.gh>
2024-05-17 18:50:04 +0100

    Task 2

--- arrayManipulation.js
+++ arrayManipulation.js
@@ -4,5 +4,16 @@ function processArray(numArray) {
     );
 }
 
+
+function formatArrayStrings(strArray , numArray) {
+    return strArray.map ((str, index) =>
+        (numArray[index] % 2 === 0 ? str.toUpperCase() : str.toLowerCase())
+    );
+
+}
+
 let selection = processArray([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
 console.log(selection);
+
+let wordSelection = formatArrayStrings(['Tuvalu' , 'Timor-Leste' , 'Bhutan', 'Belize', 'Andorra', 'Kiribati', 'Vanuatu', 'Suriname', 'Palau', 'Guyana'] , [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+console.log(wordSelection);
